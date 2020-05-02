845bbefc51a60a0a4eda0d508da19e278d79debd
cephaske254 <cephaske254@gmail.com>
2020-05-02 19:34:07 -0400

    created macro t display flashed messages in login and register.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,11 +10,13 @@ from app import db
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user = User.query.filter_by(eor_(mail = login_form.username.data, username=login_form.username.data)).first()
+        user = User.query.filter_by(email = login_form.username.data, username=login_form.username.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
-    return render_template('auth/login.html',form=login_form)
+        flash('Invalid login details!')
+    title = 'Login'
+    return render_template('auth/login.html',form=login_form,title=title)
 
 @auth.route('/register',methods=['GET','POST'])
 def register():
