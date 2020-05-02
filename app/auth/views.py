0139ec8305d6562e50d0d91dc6c1ce59476c77a7
cephaske254 <cephaske254@gmail.com>
2020-05-02 18:55:20 -0400

    created login created error handler for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,19 @@
-from flask import render_template,url_for,redirect,flash
+from flask import render_template,url_for,redirect,flash,request
 from . import auth
 from .forms import LoginForm,RegisterForm
 from ..models import User
-from flask_login import login_required
+from flask_login import login_required,login_user
+from sqlalchemy import or_
 from app import db
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(eor_(mail = login_form.username.data, username=login_form.username.data)).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
     return render_template('auth/login.html',form=login_form)
 
 @auth.route('/register',methods=['GET','POST'])
