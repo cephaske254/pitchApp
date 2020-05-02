2961c61793c7c0a3586e43260e67372d41ef28f6
cephaske254 <cephaske254@gmail.com>
2020-05-02 18:31:52 -0400

    created signup error handles

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,26 @@
-from flask import render_template
+from flask import render_template,url_for,redirect,flash
 from . import auth
 from .forms import LoginForm,RegisterForm
+from ..models import User
+from flask_login import login_required
+from app import db
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
     return render_template('auth/login.html',form=login_form)
 
-@auth.route('/register')
+@auth.route('/register',methods=['GET','POST'])
 def register():
     register_form = RegisterForm()
-    return render_template('auth/register.html',form=register_form)
+    if register_form.validate_on_submit():
+        username = register_form.username.data
+        email = register_form.email.data
+        password = register_form.password.data
+        user = User(username=username,password=password,email=email)
+        db.session.add(user)
+        db.session.commit()
+        redirect(url_for('.login'))
+    flash('Check Your Details!')
+
+    return render_template('auth/register.html',form=register_form, title='Sign Up')
--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ def load_user(user_id):
     return User.query(int(user_id))
 
 class User(UserMixin,db.Model):
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True, unique=True)
     email = db.Column(db.String(255), index=True, unique=True)
@@ -15,10 +16,14 @@ class User(UserMixin,db.Model):
 
     @property
     def password(self):
-        raise AttributeError('Password Cannot Be Read!')
+        raise AttributeError('You cannnot read the password attribute')
 
     @password.setter
-    def password(self,password):
-        generate_password_hash(passw0pord)
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
     def verify_password(self,password):
         return check_password_hash(self.password_hash,password)
+        
+    def __repr__(self):
+        return f'User - {self.username}'
