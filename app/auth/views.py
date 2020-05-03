a615bff272ff50effcce1979bd7c9c3ebd4211f6
cephaske254 <cephaske254@gmail.com>
2020-05-03 08:34:35 -0400

    created redirect in login.html if the user is already logged in

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,19 +2,26 @@ from flask import render_template,url_for,redirect,flash,request
 from . import auth
 from .forms import LoginForm,RegisterForm
 from ..models import User
-from flask_login import login_required,login_user
+from flask_login import login_required,login_user,logout_user,current_user
 from sqlalchemy import or_
 from app import db
+from flask_login.mixins import UserMixin
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        flash('You are logged In')
+        return redirect (url_for('main.index'))
+
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user = User.query.filter_by(email = login_form.username.data, username=login_form.username.data).first()
+        user = User.query.filter_by(username=login_form.username.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid login details!')
+
+    
     title = 'Login'
     return render_template('auth/login.html',form=login_form,title=title)
 
@@ -28,7 +35,12 @@ def register():
         user = User(username=username,password=password,email=email)
         db.session.add(user)
         db.session.commit()
-        redirect(url_for('.login'))
-    flash('Check Your Details!')
-
+        return redirect(url_for('.login'))
     return render_template('auth/register.html',form=register_form, title='Sign Up')
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Youve been logged out!')
+    return redirect(url_for('main.index'))
