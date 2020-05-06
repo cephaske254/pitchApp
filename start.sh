2422e5b689acce0c443ddba13233b3c4680aeca4
cephaske254 <cephaske254@gmail.com>
2020-05-06 09:54:12 +0300

    heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 export SECRET_KEY=cephaske254
 export MAIL_USERNAME=cephaske254@gmail.com
 export MAIL_PASSWORD=admin121
+export DATABASE_URL=postgres://olfjsakulknjhf:042e17f0ae14ee30
 
 python3 manage.py server
