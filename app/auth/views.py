ef5afcad3ab06296e47cdcfcc0d918f398d0a634
cephaske254 <cephaske254@gmail.com>
2020-05-06 15:22:22 +0300

    heroku

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,8 +36,9 @@ def register():
         user = User(username=username,password=password,email=email)
         db.session.add(user)
         db.session.commit()
+        mail_message('Welcome To Pitch-Advance!','email/welcome_user',user.email,user=user)
         return redirect (url_for('auth.login'))
-        
+
     return render_template('auth/register.html',form=register_form, title='Sign Up')
 
 @auth.route('/logout')
