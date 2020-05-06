fde0db564d48107b0b25a8ba7e31bb53a88aa9e1
cephaske254 <cephaske254@gmail.com>
2020-05-06 11:19:57 +0300

    hreoku

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export SECRET_KEY=cephaske254
 export MAIL_USERNAME=cephaske254@gmail.com
 export MAIL_PASSWORD=admin121
-export DATABASE_URL=postgres://olfjsakulknjhf:042e17f0ae14ee30
+export DATABASE_URL=postgres://olfjsakulknjhf:042e17f0ae14ee306e5cce57b9cab04fd27079a2c911ad5703268912602aecde@ec2-18-206-84-251.compute-1.amazonaws.com:5432/d2mfoqt6l22tgf
 
 python3 manage.py server
